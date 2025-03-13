e08b2a062fba13c70acd177da547ec53b8954118
Deep3713 <145797364+deepakkumar8400@users.noreply.github.com>
2025-03-13 12:16:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ['https://dpkjob3713.onrender.com', '*'], // Corrected format
+    origin: ['*'], // Corrected format
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
