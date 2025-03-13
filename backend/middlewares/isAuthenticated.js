a1e04f640713c30cf999043572b5816be48d0967
Deep3713 <145797364+deepakkumar8400@users.noreply.github.com>
2025-03-13 12:29:52 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -7,26 +7,19 @@ const isAuthenticated = async (req, res, next) => {
             return res.status(401).json({
                 message: "User not authenticated",
                 success: false,
-            });
+            })
         }
-
-        const decode = jwt.verify(token, process.env.SECRET_KEY);
-        if (!decode) {
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
             return res.status(401).json({
-                message: "Invalid token",
-                success: false
-            });
-        }
-
+                message:"Invalid token",
+                success:false
+            })
+        };
         req.id = decode.userId;
         next();
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
-            message: "Internal Server Error",
-            success: false
-        });
     }
-};
-
+}
 export default isAuthenticated;
