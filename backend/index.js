62eb1b21afdd6dfafa921bb0747981e97cf2e3c2
Deep3713 <145797364+deepakkumar8400@users.noreply.github.com>
2025-03-13 12:05:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,31 +12,28 @@ dotenv.config({});
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'[https://dpkjob3713.onrender.com,'*']',
+    origin: ['https://dpkjob3713.onrender.com', '*'], // Corrected format
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials:true
-}
-
+    credentials: true
+};
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// API Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
