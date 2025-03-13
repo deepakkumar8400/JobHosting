60b529b8bf1f8d0226ed3ce96157c64f9efdbbfe
Deep3713 <145797364+deepakkumar8400@users.noreply.github.com>
2025-03-13 09:13:19 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 export const USER_API_END_POINT="https://jobhosting-backend.onrender.com/api/v1/user";
 export const JOB_API_END_POINT="https://jobhosting-backend.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://jobhosting-backend.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://jobhosting-backend.onrender.com/api/v1/company";
+=======
+// Some other changes from the remote repository
+>>>>>>> main
