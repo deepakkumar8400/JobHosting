62aeb66efcbe777f351e31bd241cad121e75d957
Deep3713 <145797364+deepakkumar8400@users.noreply.github.com>
2025-03-17 16:40:21 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import passport from "passport";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { singleUpload } from "../middlewares/multer.js";
+import { singleUpload } from "../middlewares/mutler.js";
 
 const router = express.Router();
 
